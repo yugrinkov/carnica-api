dd0e1bb0fa94d68bef7bc4a93a46fab374c813bc
Yurii Grynkov <yugrinkov@gmail.com>
2020-04-13 20:16:34 +0300

    Update saveToken.js

--- saveToken.js
+++ saveToken.js
@@ -4,7 +4,7 @@ const travisToken = process.env.TRAVIS_API_TOKEN;
 if (travisToken) {
   fs.writeFile(
     `admin/src/containers/Admin/token.js`,
-    `window.token = ${process.env.TRAVIS_API_TOKEN}`,
+    `window.token = "${process.env.TRAVIS_API_TOKEN}";`,
     "utf8",
     (err) => {}
   );
